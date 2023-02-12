183ede8766168864f7b813b653a4d7d831ec5263
Yuriy <focus.us3r@gmail.com>
2023-02-12 14:36:24 +0100

    homework for the eighth lesson

--- eight.py
+++ eight.py
@@ -3,5 +3,7 @@ ch = input("Введіть символ:")
 start = 0
 while start < len(s):
     poi = s.find(ch, start)
+    if poi == -1:
+        break
     print('index of symbol: ', poi)
     start = poi + 1
