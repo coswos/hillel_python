61b63e5cd3bfb7f27432791efae4570aa17bad05
Yuriy <focus.us3r@gmail.com>
2023-02-19 12:20:23 +0100

    second way to do it

--- eleven.py
+++ eleven.py
@@ -1,10 +1,9 @@
 body = [x for x in range(10)]
-k = int(input('please enter the index to be removed from the list:')) #index
+k = int(input('please enter the index to be removed from the list:'))  # index
 print(body)
 
-
-tmp = body[0:k] + body[k+1:len(body)]
-body = tmp
+# tmp = body[0:k] + body[k+1:len(body)]
+# body = tmp
+body[k:-1] = body[k + 1:-1]
 
 print(body)
-
