dfbc6e475b5296e7a086573827f2db816c998aab
Yuriy <focus.us3r@gmail.com>
2023-03-09 16:32:14 +0100

    rewrited

--- seventeenth.py
+++ seventeenth.py
@@ -1,22 +1,17 @@
 num = int(input('please enter a number greater than zero: '))
 
-if num <= 0:
-    raise Exception('number less than zero or zero')
+def number_collaps(num):
+    if num <= 0:
+        raise Exception('number less than zero or zero')
+        # return "zero division"
 
-last_num = num % 10  # последнее число (единицы)
-ten = f'{(num % 100) // 10}0'  # целый десяток
-hundred = f'{(num % 1000) // 100}00'  # сотни
-k = f'{num // 1000}000'  # много тысяч
-
-
-def print_all(num=num, k=k, hundred=hundred, ten=ten, last=last_num):
-    print(
-        k + ' + ' if int(k) != 0 else '',
-        hundred + ' + ' if int(hundred) != 0 else '',
-        ten + ' + ' if int(ten) != 0 else '',
-        last if int(last) != 0 else '',
-        sep=''
-    )
+    number = str(num)
+    list1 = []
+    for i in range(len(number)):
+        if number[i] != "0":
+            list1.append(number[i] + "0" * (len(number) - (i+1)))
+    return "+".join(list1)
 
 
-print_all()
+print(number_collaps(num))
+
